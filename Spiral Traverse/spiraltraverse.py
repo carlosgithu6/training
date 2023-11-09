dbd5949c97afafa99bf4ce76e3fc93c9d098ee10
carlos.besil <carlos.besil@gamil.com>
2023-11-09 11:08:12 -0600

    spiral traverse using set instead  of list

--- Spiral Traverse/spiraltraverse.py
+++ Spiral Traverse/spiraltraverse.py
@@ -1,49 +1,44 @@
 def spiralTraverse(array):
     rows =  len(array)
     cols = len(array[0])
-    print(array[1][1])
     total_elem = rows * cols
     counter = 0
     result = []
-    visited = []
+    visited = set()
     i_row = 0
     i_col = 0
     while counter < total_elem:
-      
-      
+            
         while i_col < cols and ((i_row,i_col) not in visited) :
-            visited.append((i_row,i_col))
+            visited.add((i_row,i_col))
             result.append(array[i_row][i_col])
             counter += 1
             i_col += 1
         i_row += 1
         i_col -= 1
         while i_row < rows and ((i_row,i_col) not in visited) :
-            visited.append((i_row,i_col))
+            visited.add((i_row,i_col))
             result.append(array[i_row][i_col])
             counter += 1
             i_row += 1
         i_col -= 1
         i_row -= 1
         while i_col >= 0 and ((i_row,i_col) not in visited) :
-            visited.append((i_row,i_col))
+            visited.add((i_row,i_col))
             result.append(array[i_row][i_col])
             counter += 1
             i_col -= 1 
         i_row -= 1
         i_col += 1
         while i_row >= 0 and ((i_row,i_col) not in visited) :
-            visited.append((i_row,i_col))
+            visited.add((i_row,i_col))
             result.append(array[i_row][i_col])
             counter += 1
             i_row -= 1 
         
         i_col += 1
         i_row += 1
-       
-    print (visited)
-    print (result)
-
+    return result
 
 
 arr= [
